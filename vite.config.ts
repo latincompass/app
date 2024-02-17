f8f3f01b7ee02b08eeb93c1df87760b7076488b7
latincompass <latincompass@devide>
2024-02-18 05:51:16 +0800

    Auto Commit

--- vite.config.ts
+++ vite.config.ts
@@ -40,4 +40,16 @@ export default defineConfig({
       plugins: [],
     },
   },
+    build: {
+    chunkSizeWarningLimit: 1500, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      }
+    }
+    }, 
 });
